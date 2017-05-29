638aa244c0dc479287323fc820fb42c28b5b04c4
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2017-05-29 16:30:21 -0300

    cadastro de servicos. closes #4

--- config/urls.py
+++ config/urls.py
@@ -9,6 +9,7 @@ from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
 urlpatterns = [
+    url(r'^autentica/', include('chamados-cmc.autentica.urls', namespace='autentica')),
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name='about'),
 
