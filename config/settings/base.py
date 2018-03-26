a170a69fba96b9ea176f8a5aaa8db565d6c68ae9
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2018-03-26 14:52:34 -0300

    ajustes email

--- config/settings/base.py
+++ config/settings/base.py
@@ -103,10 +103,10 @@ FIXTURE_DIRS = (
 #EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_BACKEND = env('DJANGO_EMAIL_BACKEND', default='djcelery_email.backends.CeleryEmailBackend')
 EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+#EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+#EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = env('EMAIL_PORT')
-EMAIL_USE_TLS = env('EMAIL_USE_TLS')
+#EMAIL_USE_TLS = env('EMAIL_USE_TLS')
 
 # MANAGER CONFIGURATION
 # ------------------------------------------------------------------------------
