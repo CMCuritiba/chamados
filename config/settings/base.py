812e90bcca988f615520369d35fcadbe57404d37
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2018-03-28 19:15:52 -0300

    configuracoes e tratamento de excecoes

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,7 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'chamadoscmc.lib.error_handler.HandleBusinessExceptionMiddleware'
+    'autentica.lib.error_handler.HandleBusinessExceptionMiddleware'
     
 ]
 
