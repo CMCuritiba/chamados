50b508f579fc193b3cb6b55d5749b5948eea1879
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2018-04-04 15:03:23 -0300

    ajustes testes e cia

--- chamadoscmc/core/tests/factories.py
+++ chamadoscmc/core/tests/factories.py
@@ -1,6 +1,7 @@
 from factory.django import DjangoModelFactory
 
 from .. import models
+from autentica.models import User as Usuario
 
 class SetorChamadoFactory(DjangoModelFactory):
 	class Meta:
@@ -10,3 +11,16 @@ class SetorChamadoFactory(DjangoModelFactory):
 	setor_id = 171
 	recebe_chamados = True
 	localizacao = False
+
+class ServicoFactory(DjangoModelFactory):
+	class Meta:
+		model = models.Servico
+
+class GrupoServicoFactory(DjangoModelFactory):
+	class Meta:
+		model = models.GrupoServico		
+
+class UsuarioFactory(DjangoModelFactory):
+	class Meta:
+		model = Usuario
+
