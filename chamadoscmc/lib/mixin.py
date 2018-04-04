06162613b392249c33a23378935a46ab1c06ca31
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2018-04-04 10:58:37 -0300

    restricoes de acesso a fila. closes #130

--- chamadoscmc/lib/mixin.py
+++ chamadoscmc/lib/mixin.py
@@ -5,7 +5,11 @@ from braces import views
 from django.http import HttpResponseRedirect, HttpResponse
 
 from autentica.util.mixin import CMCLoginRequired, CMCAdminLoginRequired
+from ..core.models import SetorChamado
 
+#----------------------------------------------------------------------------------------------
+#
+#----------------------------------------------------------------------------------------------
 class ChamadosAdminRequired(CMCLoginRequired, CMCAdminLoginRequired):
 	message_url = '/acesso/admin'
 
@@ -15,3 +19,17 @@ class ChamadosAdminRequired(CMCLoginRequired, CMCAdminLoginRequired):
 
 		return super(CMCAdminLoginRequired, self).dispatch(
 			request, *args, **kwargs)
+
+#----------------------------------------------------------------------------------------------
+#
+#----------------------------------------------------------------------------------------------
+class ChamadosAtendenteRequired(CMCLoginRequired):
+	message_url = '/acesso/atendente'
+
+	def dispatch(self, request, *args, **kwargs):
+
+		setor_chamado = SetorChamado.objects.get(setor_id=request.session['setor_id'])
+		if not setor_chamado.recebe_chamados:
+			return HttpResponseRedirect(self.message_url)
+
+		return super(CMCLoginRequired, self).dispatch(request, *args, **kwargs)
