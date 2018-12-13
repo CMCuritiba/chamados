b5734d2b5a8587d99b874f964f8f5158a20372b9
aleodoni <aleodoni@gmail.com>
2018-12-13 08:23:36 -0200

    ajustes tunning supervisor e celery

--- fabfile.py
+++ fabfile.py
@@ -231,8 +231,15 @@ def cria_links():
 
 @task
 def restart_nginx_supervisor():
-	sudo('supervisorctl reload')
+	#sudo('supervisorctl reload')
+	sudo('supervisorctl stop celery')
+	# ainda não foi resolvido no celery processo para matar os filhos via supervisor
+	# só mata o processo 'pai', deixando os filhos zumbis
+	# o comando abaixo detona os zumbis na mão
+	sudo('pkill -f {}'.format('/usr/share/envs/chamados/bin/celery'))
+	sudo('supervisorctl reread')
 	sudo('supervisorctl restart {}'.format(PROJECT_NAME))
+	sudo('supervisorctl start celery')
 	sudo('service nginx restart')
 
 @task
