4fcaf8efcbc2e1fd99fb4e646c46f79212b258a7
aleodoni <aleodoni@gmail.com>
2019-02-27 10:11:02 -0300

    adicionado fonte ttf para imprimir caracteres especiais, correção duplicidade selecione combobox servico (closes #219)

--- chamadoscmc/core/views.py
+++ chamadoscmc/core/views.py
@@ -946,8 +946,8 @@ class ImprimeChamado(CMCReportView):
     download_filename = 'chamado.pdf'
 
     def get_context_data(self, **kwargs):
-        context = super(CMCReportView, self).get_context_data(**kwargs)
-        context['title'] = 'Informação do Chamado'
+        context = super(CMCReportView, self).get_context_data(encoding =u"utf-8", **kwargs)
+        context['title'] = 'Dados do Chamado'
         context['pagesize'] = 'A4 portrait'
         context['chamado'] = self.chamado
         context['completo'] = self.completo
@@ -963,7 +963,7 @@ class ImprimeChamado(CMCReportView):
     def get(self, request, *args, **kwargs):
         s_helper = ServiceHelper()
 
-        context = super(CMCReportView, self).get_context_data(**kwargs)
+        context = super(CMCReportView, self).get_context_data(encoding =u"utf-8", **kwargs)
         #id_chamado = kwargs.get('id_chamado', None)
         #opt = kwargs.get('opt', False)
 
